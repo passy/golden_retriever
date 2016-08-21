54d42d6c43f33dcfe61e63e36a0239adbc4a0357
Pascal Hartig <phartig@rdrei.net>
2016-08-21 15:31:30 +0100

    WIP: golden-retriever-indexer command parser

---
+++ src/indexer/app.rs
@@ -0,0 +1,13 @@
+extern crate clap;
+use self::clap::{Arg, App, SubCommand};
+
+pub fn make() -> App<'static, 'static> {
+    App::new("golden-retriever-indexer")
+        .version("0.0.1")
+        .about("Golden Retriever Indexer")
+        .subcommand(SubCommand::with_name("ls").about("list indexed directories"))
+        .subcommand(SubCommand::with_name("add").about("add a new directory to be indexed"))
+        .subcommand(SubCommand::with_name("rm").about("remove a directory from the index"))
+        .subcommand(SubCommand::with_name("status").about("show the current status of the indexer"))
+        .subcommand(SubCommand::with_name("run").about("run the indexer"))
+}
